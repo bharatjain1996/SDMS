31f58393fd23463901b84491d5cd63e4c3499166
bharatjain1996 <ck.bharatjain@gmail.com>
2018-03-26 12:49:59 +0530

    Ujju

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var myIndexFile = angular.module(myIndexFile, []);
+
+myIndexFile.controller('IndexController', ['$scope' ,function($scope)
+{
+	$scope.showitem() = function()
+	{
+      box.style.display = block;
+	}
+}]);
